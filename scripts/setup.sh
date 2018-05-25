5f26ea1588bdee4ab00a18b68a1bbd372084331b
Nick Lang <nick@nicklang.com>
2018-05-25 14:20:30 -0600

    handle some file perms before starting, also cleanup after setup completes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,9 @@
 #/bin/ash
 
+chown 1000 -R ${PWD}/config
+find ${PWD}/config -type f -name "*.keystore" -print -exec chmod go-wrx {} \;
+find ${PWD}/config -type f -name "*.yml" -print -exec chmod go-wrx {} \;
+
 docker-compose -f docker-compose.yml -f docker-compose.setup.yml up setup_elasticsearch
 
 # restart Elasticsearch so CA's take effect.
@@ -10,4 +14,6 @@ docker-compose -f docker-compose.yml -f docker-compose.setup.yml up setup_kibana
 # setup beats and apm server
 docker-compose -f docker-compose.yml -f docker-compose.setup.yml up setup_auditbeat setup_filebeat setup_heartbeat setup_metricbeat setup_packetbeat setup_apm_server
 
+docker-compose -f docker-compose.yml -f docker-compose.setup.yml down --remove-orphans
 
+echo "All setup please run: docker-compose up -d"
